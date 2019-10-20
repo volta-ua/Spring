cbe7f28b561588595f90074924933fdc4084dcf9
volta-ua <olimpiaplace@gmail.com>
2019-10-20 23:47:42 +0300

    2010-2347

--- YuriyTkachSpring/src/main/java/com/yet/spring/core/App.java
+++ YuriyTkachSpring/src/main/java/com/yet/spring/core/App.java
@@ -25,7 +25,7 @@ public class App
 		Event e=(Event)ctx.getBean("event");
 		e.setMessage("Message is set manually");
 		
-		for (int i=0;i<10;i++) {
+		for (int i=0;i<11;i++) {
 			app.logEvent(e);
 		}
 		
